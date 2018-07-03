1e0edafdabed1b5e7b3e6f4f296483f30abe8ce6
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-07-03 20:32:15 +0300

    Added mdl fixed layout drawer and header layout

--- src/Main.kt
+++ src/Main.kt
@@ -1,7 +1,20 @@
+import kotlinmdl.components.MdlLayoutTitle
+import kotlinmdl.components.dslish.fixedDrawer
+import kotlinmdl.components.dslish.fixedHeader
+import kotlinmdl.components.dslish.mdlLayout
+import kotlinmdl.components.dslish.row
 import site.app
+import site.pages.About
 
 fun main(args: Array<String>) {
     app {
+        mdlLayout("layout") {
+            val initialContent = About
+            val headerTitle = MdlLayoutTitle(initialContent.title)
+            fixedHeader { row { +headerTitle } }
+            fixedDrawer("drawer") {
 
+            }
+        }
     }
 }
