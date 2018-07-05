9500fa9b02094b89eb04444a32a55c62b4fc366b
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-07-05 19:21:18 +0300

    Added header for drawer

--- src/Main.kt
+++ src/Main.kt
@@ -3,8 +3,16 @@ import kotlinmdl.components.dslish.fixedDrawer
 import kotlinmdl.components.dslish.fixedHeader
 import kotlinmdl.components.dslish.mdlLayout
 import kotlinmdl.components.dslish.row
+import kotlinmdl.material.style.MaterialShade
+import kotlinmdl.style.MdlColor
+import kotlinx.html.b
+import kotlinx.html.dom.create
+import kotlinx.html.img
+import kotlinx.html.js.header
 import site.app
 import site.pages.About
+import site.res.static.Img
+import kotlin.browser.document
 
 fun main(args: Array<String>) {
     app {
@@ -13,7 +21,11 @@ fun main(args: Array<String>) {
             val headerTitle = MdlLayoutTitle(initialContent.title)
             fixedHeader { row { +headerTitle } }
             fixedDrawer("drawer") {
-
+                +document.create.header("drawer-header ${MdlColor.Background.BlueGrey(MaterialShade.Primary.S300())}") {
+                    img("Avatar", Img.AVATAR, "avatar")
+                    b { +"Samet Kurumahmut" }
+                    +"Mobile Developer"
+                }
             }
             content = initialContent
         }
--- src/site/res/static/Img.kt
+++ src/site/res/static/Img.kt
@@ -5,5 +5,6 @@ object Img {
     const val PATH = "img"
     const val PATH_ = "${PATH}/"
 
+    const val AVATAR = "${PATH_}avatar.png"
     const val ME = "${PATH_}me.jpg"
 }
